db710be32a52cfa8c23f4350350c53096d059003
julian-groen <julian4920@gmail.com>
2022-02-06 18:21:08 +0100

    project setup

---
+++ LibreSDK/LibreSDK.h
@@ -0,0 +1,18 @@
+//
+//  LibreSDK.h
+//  LibreSDK
+//
+//  Created by Julian Groen on 06/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibreSDK.
+FOUNDATION_EXPORT double LibreSDKVersionNumber;
+
+//! Project version string for LibreSDK.
+FOUNDATION_EXPORT const unsigned char LibreSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibreSDK/PublicHeader.h>
+
+
